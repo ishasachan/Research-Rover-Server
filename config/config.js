1e46e1da1124eddd4effbeb7feb2bba4ec0b8e2c
ishasachan <ishasachan30@gmail.com>
2023-06-24 20:50:29 +0530

    Credentials Update

--- config/config.js
+++ config/config.js
@@ -7,6 +7,6 @@ module.exports = {
   mongoURI: process.env.MONGO_URI || 'mongodb+srv://Abhi6722:Abhi6722@researchrover.ckwzho3.mongodb.net/?retryWrites=true&w=majority',
   smtpHost: process.env.SMTP_HOST || 'smtp.hostinger.com',
   smtpPort: process.env.SMTP_PORT || 465,
-  smtpUser: process.env.SMTP_USER || 'hackshark@heistat49.com',
-  smtpPassword: process.env.SMTP_PASSWORD || 'Hack@shark11',
+  smtpUser: process.env.SMTP_USER || 'researchrover@brandlime.in',
+  smtpPassword: process.env.SMTP_PASSWORD || 'Research@11',
 };
